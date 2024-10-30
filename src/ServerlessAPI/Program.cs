683199c477815519389df4361fd82fce1b11c0c5
jdsanchez93 <jdsanchez2011@gmail.com>
2024-10-30 16:04:48 -0600

    Remove dynamodb (#1)
    * remove dynamo from template
    
    * remove dynamodb from .net project
    
    * remove from test project
    
    * remove dynamo reference in comment

--- src/ServerlessAPI/Program.cs
+++ src/ServerlessAPI/Program.cs
@@ -1,8 +1,5 @@
 using System.Text.Json;
 using Amazon;
-using Amazon.DynamoDBv2;
-using Amazon.DynamoDBv2.DataModel;
-using ServerlessAPI.Repositories;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,12 +16,6 @@ builder.Services
         {
             options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
         });
-
-string region = Environment.GetEnvironmentVariable("AWS_REGION") ?? RegionEndpoint.USEast2.SystemName;
-builder.Services
-        .AddSingleton<IAmazonDynamoDB>(new AmazonDynamoDBClient(RegionEndpoint.GetBySystemName(region)))
-        .AddScoped<IDynamoDBContext, DynamoDBContext>()
-        .AddScoped<IBookRepository, BookRepository>();
 
 // Add AWS Lambda support. When running the application as an AWS Serverless application, Kestrel is replaced
 // with a Lambda function contained in the Amazon.Lambda.AspNetCoreServer package, which marshals the request into the ASP.NET Core hosting framework.
