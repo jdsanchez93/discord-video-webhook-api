6cb3599c76811e4e91c75673981b951b59d2bc22
jd <jdsanchez2011@gmail.com>
2024-10-30 15:56:58 -0600

    remove dynamodb from .net project

--- src/ServerlessAPI/Controllers/BooksController.cs
+++ src/ServerlessAPI/Controllers/BooksController.cs
@@ -1,7 +1,5 @@
-using Amazon.DynamoDBv2.DataModel;
 using Microsoft.AspNetCore.Mvc;
 using ServerlessAPI.Entities;
-using ServerlessAPI.Repositories;
 
 namespace ServerlessAPI.Controllers;
 
@@ -10,95 +8,19 @@ namespace ServerlessAPI.Controllers;
 public class BooksController : ControllerBase
 {
     private readonly ILogger<BooksController> logger;
-    private readonly IBookRepository bookRepository;
 
-    public BooksController(ILogger<BooksController> logger, IBookRepository bookRepository)
+    public BooksController(ILogger<BooksController> logger)
     {
         this.logger = logger;
-        this.bookRepository = bookRepository;
     }
 
     // GET api/books
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Book>>> Get([FromQuery] int limit = 10)
+    public ActionResult<IEnumerable<Book>> Get([FromQuery] int limit = 10)
     {
         if (limit <= 0 || limit > 100) return BadRequest("The limit should been between [1-100]");
 
-        return Ok(await bookRepository.GetBooksAsync(limit));
+        return Ok(new Book());
     }
 
-    // GET api/books/5
-    [HttpGet("{id}")]
-    public async Task<ActionResult<Book>> Get(Guid id)
-    {
-        var result = await bookRepository.GetByIdAsync(id);
-
-        if (result == null)
-        {
-            return NotFound();
-        }
-
-        return Ok(result);
-    }
-
-    // POST api/books
-    [HttpPost]
-    public async Task<ActionResult<Book>> Post([FromBody] Book book)
-    {
-        if (book == null) return ValidationProblem("Invalid input! Book not informed");
-
-        var result = await bookRepository.CreateAsync(book);
-
-        if (result)
-        {
-            return CreatedAtAction(
-                nameof(Get),
-                new { id = book.Id },
-                book);
-        }
-        else
-        {
-            return BadRequest("Fail to persist");
-        }
-
-    }
-
-    // PUT api/books/5
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Put(Guid id, [FromBody] Book book)
-    {
-        if (id == Guid.Empty || book == null) return ValidationProblem("Invalid request payload");
-
-        // Retrieve the book.
-        var bookRetrieved = await bookRepository.GetByIdAsync(id);
-
-        if (bookRetrieved == null)
-        {
-            var errorMsg = $"Invalid input! No book found with id:{id}";
-            return NotFound(errorMsg);
-        }
-
-        book.Id = bookRetrieved.Id;
-
-        await bookRepository.UpdateAsync(book);
-        return Ok();
-    }
-
-    // DELETE api/books/5
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> Delete(Guid id)
-    {
-        if (id == Guid.Empty) return ValidationProblem("Invalid request payload");
-
-        var bookRetrieved = await bookRepository.GetByIdAsync(id);
-
-        if (bookRetrieved == null)
-        {
-            var errorMsg = $"Invalid input! No book found with id:{id}";
-            return NotFound(errorMsg);
-        }
-
-        await bookRepository.DeleteAsync(bookRetrieved);
-        return Ok();
-    }
 }
