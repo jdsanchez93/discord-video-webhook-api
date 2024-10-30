21b7d980a8b5c706c25d725cd5f81db6b6e6f5c6
jd <jdsanchez2011@gmail.com>
2024-10-30 16:03:23 -0600

    remove dynamo reference in comment

--- src/ServerlessAPI/Entities/Book.cs
+++ src/ServerlessAPI/Entities/Book.cs
@@ -1,9 +1,5 @@
 namespace ServerlessAPI.Entities;
 
-// <summary>
-/// Map the Book Class to DynamoDb Table
-/// To learn more visit https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DeclarativeTagsList.html
-/// </summary>
 public class Book
 {
     public Guid Id { get; set; } = Guid.Empty;
